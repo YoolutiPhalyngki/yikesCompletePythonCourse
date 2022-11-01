53ebd00b1ed76949a96af16bef79edb5bd63423a
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-01 13:55:23 +0000

    Single element tuple ending with comma (,)

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 short_tuple ="Rolf", "Bob"
 a_bit_clearer = ("Rolf", "Bob")
 not_a_tuple = "Rolf"
+this_is_a_tuple = "Rolf",
 
 print(short_tuple)
 print(a_bit_clearer)
 print(not_a_tuple)
+print(this_is_a_tuple)
