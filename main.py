89c50a6153196eea5a8d555697791b3dcf186871
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-01 13:12:31 +0000

    Use append() function to add value to the list

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 # Learning List
-friends = [
-  ["Rolf", 24], 
-  ["Bob", 30], 
-  ["Anne", 27],
-  ["Charlie", 37],
-  ["Jen", 25],
-  ["Adam", 29],
-]
+friends = ["Rolf", "Bob", "Anne"]
+friends.append("Jen")
 
 print(friends)
