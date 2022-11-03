59091d5553685cb5ca81df601e352331ead6f9e8
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 11:33:10 +0000

    Using join() function to join the elements of the list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Learning joining a lists
 
 friends = ["Rolf", "Anne", "Charlie"]
-print(f"My friends are {friends}.")
+comma_separated = ", ".join(friends)
+print(f"My friends are {comma_separated}.")
