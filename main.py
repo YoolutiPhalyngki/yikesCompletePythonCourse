09473c3301558f0d9a5654948729e55b8c790e40
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 08:00:06 +0000

    symmetric_difference() function give not in both sets

--- main.py
+++ main.py
@@ -2,8 +2,5 @@
 art_friends = {"Rolf", "Anne", "Jen"}
 science_friends = {"Jen", "Charlie"}
 
-art_but_not_science = art_friends.difference(science_friends)
-science_but_not_art = science_friends.difference(art_friends)
-
-print(art_but_not_science)
-print(science_but_not_art)
+not_in_both = art_friends.symmetric_difference(science_friends)
+print(not_in_both)
