2f34d6f85ae995a3ccb0b9e7b2aad2c58d7ad783
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 09:00:06 +0000

    using dict() function to convert list of tuple data into dictionary

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 # Learning Dictionary
-friends = (
-  {"name": "Rolf Smith", "age": 24},
-  {"name": "Adam Wool", "age": 30},
-  {"name": "Anne Pun", "age": 27}
-)
+friends = [("Rolf", 24), ("Adam", 30), ("Anne", 27)]
 
-print(friends[0]["name"])
-print(friends[1]["name"])
-print(friends[2]["name"])
+friend_ages = dict(friends)
+print(friend_ages)
