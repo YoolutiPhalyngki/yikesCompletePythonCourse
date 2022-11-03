85a6612cb2e93107d503bdcc330d428a32bbdcd1
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 11:28:16 +0000

    Another updated solution for the problem on lists and sets

--- main.py
+++ main.py
@@ -1,24 +1,37 @@
 # Problem on lists, set
 lottery_numbers = {13, 21, 22, 5, 8}
-
+ 
+"""
+A player looks like this:
+ 
+{
+    'name': 'PLAYER_NAME',
+    'numbers': {1, 2, 3, 4, 5}
+}
+ 
+Define a list with two players (you can come up with their names and numbers).
+"""
 players = [
-  {
-    'name': "Joe",
-    'number': {1, 7, 88, 8}
-  }, 
-  {
-    'name': "Mac",
-    'number': { 2, 21, 3, 31, 8, 77, 5}
-  }
+    {
+        "name": "Rolf",
+        "numbers": {1, 3, 8, 22, 21}
+    },
+    {
+        "name": "Jose",
+        "numbers": {4, 9, 10, 12, 15}
+    }
 ]
-
-player1_count = len(players[0]['number'].intersection(lottery_numbers))
-
-player2_count = len(players[1]['number'].intersection(lottery_numbers))
-
-print(players[0]['name'],player1_count)
-print(players[1]['name'],player2_count)
-
-print(f"Player {players[0]['name']} got {player1_count} numbers right")
-
-print(f"Player {players[1]['name']} got {player2_count} numbers right")
+ 
+# For each of the two players, print out a string like this: "Player PLAYER_NAME got 3 numbers right.".
+# Of course, replace PLAYER_NAME by their name, and the 3 by the amount of numbers they matched with lottery_numbers.
+# You'll have to access each player's name and numbers, and calculate the intersection of their numbers with lottery_numbers.
+# Then construct a string and print it out.
+ 
+# Remember: the string must contain the player's name and the amount of numbers they got right!
+name = players[0]["name"]
+numbers = players[0]["numbers"].intersection(lottery_numbers)
+print(f"Player {name} got {len(numbers)} numbers right.")
+ 
+name = players[1]["name"]
+numbers = players[1]["numbers"].intersection(lottery_numbers)
+print(f"Player {name} got {len(numbers)} numbers right.")
