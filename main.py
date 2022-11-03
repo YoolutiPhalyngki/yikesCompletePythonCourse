57fd74c5819c5700f4931193868e1bb1b5e3e32d
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 08:05:07 +0000

    intersection() -> gets members that are in both sets

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 art_friends = {"Rolf", "Anne", "Jen"}
 science_friends = {"Jen", "Charlie"}
 
-not_in_both = art_friends.symmetric_difference(science_friends)
-print(not_in_both)
+art_and_science = art_friends.intersection(science_friends)
+print(art_and_science)
