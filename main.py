7921afbc6f8cc011c7d88de586f86c3945b7b0e3
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 08:57:21 +0000

    accessing dictionary within tuple

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Learning Dictionary
-friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
+friends = (
+  {"name": "Rolf Smith", "age": 24},
+  {"name": "Adam Wool", "age": 30},
+  {"name": "Anne Pun", "age": 27}
+)
 
-friend_ages["Bob"] = 20
-friend_ages["Rolf"] = 25
-
-print(friend_ages)
+print(friends[0]["name"])
+print(friends[1]["name"])
+print(friends[2]["name"])
