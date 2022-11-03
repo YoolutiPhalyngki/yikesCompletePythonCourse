452e0aaed847df2856e701dae11304f2e29ac040
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 13:46:26 +0000

    if-elif-else chain statements

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # Learning if statement
-friend = "Rolf"
+friends = ["Rolf", "Bob", "Anne"]
+family = ["Jen", "Charlie"]
+
 user_name = input("Enter your name: ")
 
-if user_name == friend:
+if user_name in friends:
   print("Hello, friend!")
+elif user_name in family:
+  print("Hello, family!")
 else:
-  print("Hello, stranger!")
+  print("I don't know you.")
