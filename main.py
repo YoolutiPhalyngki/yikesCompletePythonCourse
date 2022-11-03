b3cc9934fb30fd041f0181a0efb91834e15c423d
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 11:21:03 +0000

    Solution to problem on lists and sets

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
-# Learning sum and len function
-grades = [80, 75, 90, 100]
+# Problem on lists, set
+lottery_numbers = {13, 21, 22, 5, 8}
 
-total = sum(grades)
-length = len(grades)
+players = [
+  {
+    'name': "Joe",
+    'number': {1, 7, 88, 8}
+  }, 
+  {
+    'name': "Mac",
+    'number': { 2, 21, 3, 31, 8, 77, 5}
+  }
+]
 
-average = total / length
-print(average)
+player1_count = len(players[0]['number'].intersection(lottery_numbers))
+
+player2_count = len(players[1]['number'].intersection(lottery_numbers))
+
+print(players[0]['name'],player1_count)
+print(players[1]['name'],player2_count)
+
+print(f"Player {players[0]['name']} got {player1_count} numbers right")
+
+print(f"Player {players[1]['name']} got {player2_count} numbers right")
