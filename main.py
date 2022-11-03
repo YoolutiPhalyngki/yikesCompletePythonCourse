a7bb11dcc0874544b57a04c7e48868fae7ffd6f9
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-03 07:56:25 +0000

    intro to advance set operations

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 # Learning Set
-art_friends = {"Rolf", "Anne"}
+art_friends = {"Rolf", "Anne", "Jen"}
 science_friends = {"Jen", "Charlie"}
 
-art_friends.add("Jen")
+art_but_not_science = art_friends.difference(science_friends)
+science_but_not_art = science_friends.difference(art_friends)
 
-print(art_friends)
-
-art_friends.remove("Jen")
-
-print(art_friends)
+print(art_but_not_science)
+print(science_but_not_art)
