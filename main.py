e1a52f6e0930a9dccd670b05da78b980237831d1
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-04 13:37:53 +0000

    another example of destructuring multiple variable assignment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Learning destructuring
 
-currencies = 0.8, 1.2
-usd, eur = currencies
-print(usd, eur)
+friends = [("Rolf", 25), ("Anne", 37), ("Charlie", 31), ("Bob", 22)]
+
+for name, age in friends:
+  print(f"{name} is {age} years old.")
