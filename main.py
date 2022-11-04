50c681caf21879ea498e79e3089cc561334363f8
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-04 15:21:35 +0000

    intro about iterating over dictionary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Learning destructuring
 
-friends = [("Rolf", 25), ("Anne", 37), ("Charlie", 31), ("Bob", 22)]
+friend_ages = {"Rolf": 25, "Anne": 37, "Charlie": 31, "Bob": 22}
 
-for name, age in friends:
-  print(f"{name} is {age} years old.")
+for name in friend_ages:
+  print(name)
