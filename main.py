e0ce284141f896768efaadb2b08ff58dfe947d6e
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-04 15:25:48 +0000

    iterate over both keys and values of a dictionary

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 
 friend_ages = {"Rolf": 25, "Anne": 37, "Charlie": 31, "Bob": 22}
 
-for age in friend_ages.values():
-  print(age)
+for name, age in friend_ages.items():
+  print(f"{name} is {age} years old.")
