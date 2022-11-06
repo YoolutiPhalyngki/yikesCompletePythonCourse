c3e4cd2b8d2be78b711a8aaebd03b2ad7bf8f87d
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-06 18:12:29 +0000

    you can used else with for loop, and it would execute only if there is no break on the for loop

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # Learning else with loop
 
-cars = ["ok", "ok", "ok", "faulty", "ok", "ok"]
-all_successful = True
+cars = ["ok", "ok", "ok", "ok", "ok"]
 
 for status in cars:
   if status == "faulty":
     print("Stopping the production line!")
-    all_successful = False
     break
   print(f"This car is {status}.")
   print("Shipping new cat to customer!")
-
-if all_successful:
+else:
   print("All cars built successfully. No faulty cars!")
