17664362c55775fcfc8ad5bc87dfd150b18dc929
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 07:40:31 +0000

    finding prime number using for loop and else

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-# Learning else with loop
+# Finding prime number with loops
 
-cars = ["ok", "ok", "ok", "ok", "ok"]
-
-for status in cars:
-  if status == "faulty":
-    print("Stopping the production line!")
-    break
-  print(f"This car is {status}.")
-  print("Shipping new cat to customer!")
-else:
-  print("All cars built successfully. No faulty cars!")
+for n in range(2, 10):
+  for x in range(2, n):
+    if n % x == 0:
+      print(f"{n} equals {x} * {n//x}")
+      break
+  else:
+    print(f"{n} is a prime number.")
