64ce66ea6a6f5d1090305031797e96f1e4cde4f7
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 08:24:22 +0000

    list comprehension example

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # List comprehension in Python
 
 numbers = [0, 1, 2, 3, 4]
-doubled_numbers = []
 
-for number in numbers:
-  doubled_numbers.append(number * 2)
+doubled_numbers = [number * 2 for number in numbers]
 
 print(doubled_numbers)
