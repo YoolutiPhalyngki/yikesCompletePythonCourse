81adb4130012bf7e60b0b979490d531c0dacdd1c
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 18:50:04 +0000

    Another example of set comprehension

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ guests = ["jose", "Bob", "Rolf", "Charlie", "micheal"]
 friends_lower = {n.lower() for n in friends}
 guests_lower = {n.lower() for n in guests}
 
-print(friends_lower.intersection(guests_lower))
+present_friends = friends_lower.intersection(guests_lower)
+present_friends = {name.title() for name in present_friends}
+
+print(present_friends)
