076b99794a29943164b365e3d5de9c4d00b8953d
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 19:04:09 +0000

    intro to zip function

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# Set and dictionary comprehension
+# zip() funtion
 
 friends = ["Rolf", "Bob", "Jen", "Anne"]
 time_since_seen = [3, 7, 15, 11]
 
-long_timers = {
-  friends[i]: time_since_seen[i]
-  for i in range(len(friends))
-  if time_since_seen[i] > 5
-}
+long_timers = list(zip(friends, time_since_seen))
 
 print(long_timers)
