0301857fae383807700caebae3f46988403eef06
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 08:21:05 +0000

    intro to list comprehension

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# List slicing in Python
+# List comprehension in Python
 
-friends = ["Rolf", "Charlie", "Anna", "Bob", "Jen"]
+numbers = [0, 1, 2, 3, 4]
+doubled_numbers = []
 
-print(friends[-1:])
+for number in numbers:
+  doubled_numbers.append(number * 2)
+
+print(doubled_numbers)
