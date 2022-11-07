1f88628c61ec7455f90c2b4e4745575e0a507343
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 18:51:54 +0000

    Another example

--- main.py
+++ main.py
@@ -3,10 +3,9 @@
 friends = ["Rolf", "ruth", "charlie", "Jen"]
 guests = ["jose", "Bob", "Rolf", "Charlie", "micheal"]
 
-friends_lower = {n.lower() for n in friends}
-guests_lower = {n.lower() for n in guests}
+friends_lower = {n.title() for n in friends}
+guests_lower = {n.title() for n in guests}
 
 present_friends = friends_lower.intersection(guests_lower)
-present_friends = {name.title() for name in present_friends}
 
 print(present_friends)
