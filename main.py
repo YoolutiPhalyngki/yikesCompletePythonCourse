ea80e85c7da064a6b1fd6e3f2f90e62586781153
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 15:23:41 +0000

    you can split list comprehension into multiple lines -> what you want to keep in the list, then the loop, and finally the condition base upon.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ guests = ["jose", "Bob", "Rolf", "Charlie", "micheal"]
 friends_lower = set([f.lower() for f in friends])
 
 present_friends = [
-  name.title() for name in guests if name.lower() in friends_lower
+  name.title() 
+  for name in guests 
+  if name.lower() in friends_lower
 ]
 print(present_friends)
