4e566fc1922603d3bf9c7aa9db11617f7ef264a3
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 19:05:53 +0000

    zip() combine two list or more into a list of tuple. Use dict to convert the list element to dictionary

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 friends = ["Rolf", "Bob", "Jen", "Anne"]
 time_since_seen = [3, 7, 15, 11]
 
-long_timers = list(zip(friends, time_since_seen))
+long_timers = dict(zip(friends, time_since_seen))
 
 print(long_timers)
