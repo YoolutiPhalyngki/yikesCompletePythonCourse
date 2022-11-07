65cda3e538e305d12423539c257aab6c7905eef4
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 10:31:21 +0000

    another list comprehension example with string

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # List comprehension in Python
 
-doubled_numbers = [number * 2 for number in range(5)]
+friend_ages = [22, 31, 35, 37]
+age_strings = [f"My friend is {age} years old." for age in friend_ages]
 
-print(doubled_numbers)
+print(age_strings)
