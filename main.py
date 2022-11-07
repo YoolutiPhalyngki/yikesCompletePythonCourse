67e1b29cf8180483bc8a8fd9e39c9a3111252c67
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 15:16:44 +0000

    another example with set and list comprehension

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # List comprehension with conditions
 
-ages = [22, 35, 27, 21, 20]
-odds = [age for age in ages if age % 2 == 1]
+friends = ["Rolf", "ruth", "charlie", "Jen"]
+guests = ["jose", "Bob", "Rolf", "Charlie", "micheal"]
 
-print(odds)
+friends_lower = set([f.lower() for f in friends])
+guests_lower = set([g.lower() for g in guests])
+
+print(friends_lower.intersection(guests_lower))
