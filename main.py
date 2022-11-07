9f33de4675102348096a3ad033e1abd2fb0e5962
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-07 15:20:15 +0000

    another example

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ friends = ["Rolf", "ruth", "charlie", "Jen"]
 guests = ["jose", "Bob", "Rolf", "Charlie", "micheal"]
 
 friends_lower = set([f.lower() for f in friends])
-guests_lower = set([g.lower() for g in guests])
 
-print(friends_lower.intersection(guests_lower))
+present_friends = [
+  name.title() for name in guests if name.lower() in friends_lower
+]
+print(present_friends)
