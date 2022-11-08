ed9ea635723cea6645dcbe53eb5b307955b9a94d
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-08 06:34:05 +0000

    enumerate() function with list

--- main.py
+++ main.py
@@ -2,16 +2,9 @@
 
 friends = ["Rolf", "John", "Anna"]
 
-counter = 0
-
-for friend in friends:
-  print(counter)
-  print(friend)
-  index = counter + 1
-
-
 for counter, friend in enumerate(friends):
   print(counter)
   print(friend)
-  
+
+print(list(enumerate(friends)))
   
