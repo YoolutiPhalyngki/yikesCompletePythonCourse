a5ad4b450022315d3a231668d1291d410f2116c0
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-08 13:13:14 +0000

    improve version with passing arguments

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # Learning Arguments and Parameters
 
-car = {
-  "make": "Ford",
-  "model": "Fiesta",
-  "mileage": 23000,
-  "fuel_consumed": 460
-}
+cars = [
+  {"make": "Ford", "model": "Fiesta", "mileage": 23000, "fuel_consumed": 460},
+  {"make": "Ford", "model": "Focus", "mileage": 17000, "fuel_consumed": 350},
+  {"make": "Mazda", "model": "MX-5", "mileage": 49000, "fuel_consumed": 900},
+  {"make": "Mini", "model": "Cooper", "mileage": 31000, "fuel_consumed": 235},
+]
 
-def calculate_mpg():
-  mpg = car["mileage"] / car["fuel_consumed"]
-  name = f"{car['make']} {car['model']}"
+def calculate_mpg(car_to_calculate):
+  mpg = car_to_calculate["mileage"] / car_to_calculate["fuel_consumed"]
+  name = f"{car_to_calculate['make']} {car_to_calculate['model']}"
   print(f"{name} does {mpg} miles per gallon.")
 
-calculate_mpg()
+calculate_mpg(cars[1])
