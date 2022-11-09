efc1cca50ace0ea097b961e35d3c165faa769667
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-09 06:40:06 +0000

    simplify function using first-class function, mapping the user input to the function name in a dictinary

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ avg = lambda seq: sum(seq) / len(seq)
 total = lambda seq: sum(seq)
 top = lambda seq: max(seq)
 
+operations = {
+  "average": avg,
+  "total": total,
+  "top": top
+}
+
 students =[
   {"name": "Rolf", "grades": (67, 90, 95, 100)},
   {"name": "Bob", "grades": (56, 78, 80, 90)},
@@ -18,9 +24,5 @@ for student in students:
   print(f"Student: {name}")
   operation = input("Enter 'average', 'total', or 'top': ")
 
-  if operation == "average":
-    print(avg(grades))
-  elif operation == "total":
-    print(total(grades))
-  elif operation == "top":
-    print(top(grades))
+  operation_function = operations[operation]
+  print(operation_function(grades))
