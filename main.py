84836953d02ad1a54ae3ee3090213452f0f39eda
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-09 07:01:08 +0000

    example of lambda function with first-class function

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
 def over_age(data, getter):
   return getter(data) >= 18
 
-user = {'username': 'rolf123', 'age': 35}
+user = {'username': 'rolf123', 'age': '35'}
 
 print(over_age(user, lambda x: int(x['age'])))
