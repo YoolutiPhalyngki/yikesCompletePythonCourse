cf1115cb5a7b12ff3eb1cc275e2b646ab9afa2c8
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-09 05:23:04 +0000

    you can have multiple return in a function. Base on the circumstance, only one will run

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
 # Learning Functions and return values in Python
 
-cars = [
-  {"make": "Ford", "model": "Fiesta", "mileage": 23000, "fuel_consumed": 460},
-  {"make": "Ford", "model": "Focus", "mileage": 17000, "fuel_consumed": 350},
-  {"make": "Mazda", "model": "MX-5", "mileage": 49000, "fuel_consumed": 900},
-  {"make": "Mini", "model": "Cooper", "mileage": 31000, "fuel_consumed": 235},
-]
+def divide(x, y):
+  if y == 0:
+    return "You tried to divide by zero!"
+  else:
+    return x / y
 
-def calculate_mpg(car):
-  mpg = car["mileage"] / car["fuel_consumed"]
-  return mpg
-
-
-def car_name(car):
-  name = f"{car['make']} {car['model']}"
-  return name
-
-
-def print_car_info(car):
-  name = car_name(car)
-  mpg = calculate_mpg(car)
-  
-  print(f"{name} does {mpg} miles per gallon.")
-
-for car in cars:
-  print_car_info(car)
+print(divide(10, 2))
+print(divide(6, 0))
