b9bd69cbe5aaa128281c74afeaab1d1b286f107b
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-09 06:10:08 +0000

    before lambda function

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # Learning Lambda functions in Python
 
-d = (lambda x, y: x / y)(15, 3)
+def average(sequence):
+  return sum(sequence) / len(sequence)
 
-print(d)
+students = [
+  {"name": "Rolf", "grades": (67, 90, 95, 100)},
+  {"name": "Bob", "grades": (56, 78, 80, 90)},
+  {"name": "Jen", "grades": (98, 90, 95, 99)},
+  {"name": "Anne", "grades": (100, 100, 95, 100)},
+]
+
+for student in students:
+  print(average(student["grades"]))
 
