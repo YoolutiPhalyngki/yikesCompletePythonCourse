fb95a662401854b91c26f79094304f62ded592b6
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-09 06:59:54 +0000

    Another example of lambda with first-class function concept

--- main.py
+++ main.py
@@ -1,24 +1,8 @@
 # Learning first-class functions
 
-operations = {
-  "average": lambda seq: sum(seq) / len(seq),
-  "total": sum,
-  "top": max
-}
+def over_age(data, getter):
+  return getter(data) >= 18
 
-students =[
-  {"name": "Rolf", "grades": (67, 90, 95, 100)},
-  {"name": "Bob", "grades": (56, 78, 80, 90)},
-  {"name": "Jen", "grades": (98, 90, 95, 99)},
-  {"name": "Anne", "grades": (100, 100, 95, 100)},
-]
+user = {'username': 'rolf123', 'age': 35}
 
-for student in students:
-  name = student["name"]
-  grades = student["grades"]
-
-  print(f"Student: {name}")
-  operation = input("Enter 'average', 'total', or 'top': ")
-
-  operation_function = operations[operation]
-  print(operation_function(grades))
+print(over_age(user, lambda x: int(x['age'])))
