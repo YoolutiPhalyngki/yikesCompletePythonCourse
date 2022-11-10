ce3f82bb26a30098b028f7a48ea20670560495ab
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-10 07:52:34 +0000

    final solution of the milestone1, movie project

--- main.py
+++ main.py
@@ -2,8 +2,6 @@
 
 PROMPT = "\nEnter 'a' to add, 'l' to list, 'f' to find a movies, 'q' to quit: "
 
-user_input = input(PROMPT)
-
 movies = []
 
 
@@ -12,38 +10,50 @@ def add_new_movies():
     director = input("Enter the name of the Director: ")
     year = input("Enter the release year of the Movie: ")
 
-    data = {
+    movie_data = {
         "name": name,
         "director": director,
         "year": year
     }
 
-    movies.append(data)
+    movies.append(movie_data)
 
 
 def view_all_movies():
-    print(movies)
+    for movie in movies:
+        print_movie(movie)
+
+
+def print_movie(movie):
+    print(f"Title: {movie['name']}")
+    print(f"\tDirector: {movie['director']}")
+    print(f"\tRelease Year: {movie['year']}")
 
 
 def search_movie_info():
-    key = input("Enter the movie name to search: ")
+    search_value = input("Enter the movie name to search: ")
     for movie in movies:
-        if movie["name"] == key:
-            print(f"{movie}")
-            break
-    else:
-        print(f"There is no movie with the name {key}")
+        if movie["name"] == search_value:
+            print_movie(movie)
 
 
-while user_input != 'q':
-    if user_input == 'a':
-        add_new_movies()
-    elif user_input == 'l':
-        view_all_movies()
-    elif user_input == 'f':
-        search_movie_info()
-    else:
-        print("You entered a wrong command!")
+operations = {
+    "a": add_new_movies,
+    "l": view_all_movies,
+    "f": search_movie_info
+}
 
-    user_input = input(PROMPT)
 
+def menu():
+    selection = input(PROMPT)
+    while selection != 'q':
+        if selection in operations:
+            operation = operations[selection]
+            operation()
+        else:
+            print("You entered a wrong command!")
+
+        selection = input(PROMPT)
+
+
+menu()
