469a5d900df2044d2e2e6d06dc87b6ae59b1add4
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-10 06:48:01 +0000

    solution for milestone1, movie project use-case 1. add movie to the collection. 2. view all the movies in the collection. 3. search a movie from the existing collection.

--- main.py
+++ main.py
@@ -1,8 +1,49 @@
-# Learning first-class functions
+# Milestone 1: Movie Project
 
-def over_age(data, getter):
-  return getter(data) >= 18
+PROMPT = "\nEnter 'a' to add, 'l' to list, 'f' to find a movies, 'q' to quit: "
 
-user = {'username': 'rolf123', 'age': '35'}
+user_input = input(PROMPT)
 
-print(over_age(user, lambda x: int(x['age'])))
+movies = []
+
+
+def add_new_movies():
+    name = input("Enter the name of the Movie: ")
+    director = input("Enter the name of the Director: ")
+    year = input("Enter the release year of the Movie: ")
+
+    data = {
+        "name": name,
+        "director": director,
+        "year": year
+    }
+
+    movies.append(data)
+
+
+def view_all_movies():
+    print(movies)
+
+
+def search_movie_info():
+    key = input("Enter the movie name to search: ")
+    for movie in movies:
+        if movie["name"] == key:
+            print(f"{movie}")
+            break
+    else:
+        print(f"There is no movie with the name {key}")
+
+
+while user_input != 'q':
+    if user_input == 'a':
+        add_new_movies()
+    elif user_input == 'l':
+        view_all_movies()
+    elif user_input == 'f':
+        search_movie_info()
+    else:
+        print("You entered a wrong command!")
+
+    user_input = input(PROMPT)
+
