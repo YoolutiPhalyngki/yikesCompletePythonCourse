423feb692114ef04a4a830e0e1234bcf4f29db14
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 14:54:50 +0000

    intro to inheritance in Python

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-class Club:
-  def __init__(self, name):
+# Learning Inheritance in Python
+
+class Student:
+  def __init__(self, name, school):
     self.name = name
-    self.players = []
+    self.school = school
+    self.marks = []
 
-  def __len__(self):
-    return len(self.players)
+  def average(self):
+    return sum(self.marks) / len(self.marks)
 
-  def __getitem__(self, i):
-    return self.players[i]
+class WorkingStudent(Student):
+  def __init__(self, name, school, salary):
+    super().__init__(name, school)
+    self.salary = salary
 
-  def __repr__(self):
-    return f"Club {self.name}: {self.players}"
 
-  def __str__(self):
-    return f"Club {self.name} with {len(self.players)} players"
-
-my_club = Club('Arsenal')
-
-my_club.players.append('Rolf')
-my_club.players.append('Anne')
-
-print(len(my_club))
-print(my_club.__repr__())
-print(my_club)
-print(my_club[0])
-for club in my_club:
-  print(club)
+rolf = WorkingStudent('Rolf', 'MIT', 15.50)
+print(rolf.salary)
+rolf.marks.append(57)
+rolf.marks.append(99)
+print(rolf.average())
