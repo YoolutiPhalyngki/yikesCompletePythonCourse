8cbc36fe21a52c2e35a247c668a1d0667ab2bea5
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 18:44:54 +0000

    another example of @staticmethod

--- main.py
+++ main.py
@@ -1,26 +1,9 @@
-# Learning @classmethod and @staticmethod more example
+class Person:
+  def __init__(self, name):
+    self.name = name
 
-class FixedFloat:
-  def __init__(self, amount):
-    self.amount = amount
+  @staticmethod
+  def greet_friend(friend_name):
+    return f'Hey there, {friend_name}'
 
-  def __repr__(self):
-    return f'<FixedFloat {self.amount:.2f}>'
-
-  @classmethod
-  def from_sum(cls, value1, value2):
-    return cls(value1 + value2)
-
-new_number = FixedFloat.from_sum(19.575, 0.789)
-print(new_number)
-
-class Euro(FixedFloat):
-  def __init__(self, amount):
-    super().__init__(amount)
-    self.symbol = '₹'
-
-  def __repr__(self):
-    return f'<Euro {self.symbol}{self.amount:.2f}>'
-
-money = Euro.from_sum(16.758, 9.999)
-print(money)
+print(Person.greet_friend('Mike'))
