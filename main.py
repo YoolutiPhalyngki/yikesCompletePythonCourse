4b3a3233ff065a2248d94e4fa36db5aa7c1eeed4
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 18:35:36 +0000

    another example of @classmethod

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ class FixedFloat:
   def __repr__(self):
     return f'<FixedFloat {self.amount:.2f}>'
 
-  @staticmethod
-  def from_sum(value1, value2):
-    return FixedFloat(value1 + value2)
+  @classmethod
+  def from_sum(cls, value1, value2):
+    return cls(value1 + value2)
 
 new_number = FixedFloat.from_sum(19.575, 0.789)
 print(new_number)
