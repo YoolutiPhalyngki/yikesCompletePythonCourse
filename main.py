737e2aedd737852390e36af8abf2f85a1fe0c639
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 17:36:51 +0000

    another example

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-# Learning Inheritance in Python
-
 class Student:
   def __init__(self, name, school):
     self.name = name
     self.school = school
     self.marks = []
 
+  @property
   def average(self):
     return sum(self.marks) / len(self.marks)
 
@@ -14,11 +13,8 @@ class WorkingStudent(Student):
     super().__init__(name, school)
     self.salary = salary
 
-  @property
-  def weekly_salary(self):
-    return self.salary * 37.5
 
+rolf = WorkingStudent("Rolf", "MIT", 15.50)
+rolf.marks.append(57)
 
-rolf = WorkingStudent('Rolf', 'MIT', 15.50)
-
-print(rolf.weekly_salary)
+print(rolf.average)
