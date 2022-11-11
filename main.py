72b5d90548221d72d38d08a800af8c1e882a8c5a
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 06:52:28 +0000

    intro to Object-Oriented Programming

--- main.py
+++ main.py
@@ -1,59 +1,23 @@
-# Milestone 1: Movie Project
+# Learning Object-Oriented Programming
 
-PROMPT = "\nEnter 'a' to add, 'l' to list, 'f' to find a movies, 'q' to quit: "
-
-movies = []
-
-
-def add_new_movies():
-    name = input("Enter the name of the Movie: ")
-    director = input("Enter the name of the Director: ")
-    year = input("Enter the release year of the Movie: ")
-
-    movie_data = {
-        "name": name,
-        "director": director,
-        "year": year
-    }
-
-    movies.append(movie_data)
-
-
-def view_all_movies():
-    for movie in movies:
-        print_movie(movie)
-
-
-def print_movie(movie):
-    print(f"Title: {movie['name']}")
-    print(f"\tDirector: {movie['director']}")
-    print(f"\tRelease Year: {movie['year']}")
-
-
-def search_movie_info():
-    search_value = input("Enter the movie name to search: ")
-    for movie in movies:
-        if movie["name"] == search_value:
-            print_movie(movie)
-
-
-operations = {
-    "a": add_new_movies,
-    "l": view_all_movies,
-    "f": search_movie_info
+my_student = {
+  'name': 'Rolf Smith',
+  'grades': [70, 88, 90, 99],
+  'average': None # something here
 }
 
-
-def menu():
-    selection = input(PROMPT)
-    while selection != 'q':
-        if selection in operations:
-            operation = operations[selection]
-            operation()
-        else:
-            print("You entered a wrong command!")
-
-        selection = input(PROMPT)
+def average_grade(student):
+  return sum(student['grades']) / len(student['grades'])
 
 
-menu()
+class Student:
+  def __init__(self, new_name, new_grades):
+    self.name = new_name
+    self.grades = new_grades
+
+  def average(self):
+    return sum(self.grades) / len(self.grades)
+
+student_one = Student('Rolf Smith', [70, 88, 90, 99])
+
+print(student_one.name)
