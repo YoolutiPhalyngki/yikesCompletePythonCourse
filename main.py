623f0ae5cdb2a55c15c776de57a1bb38ad37c103
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 18:31:10 +0000

    example with inheritance

--- main.py
+++ main.py
@@ -1,35 +1,26 @@
-# Learning @classmethod and @staticmethod
+# Learning @classmethod and @staticmethod more example
 
-class Student:
-  def __init__(self, name, school):
-    self.name = name
-    self.school = school
-    self.marks = []
+class FixedFloat:
+  def __init__(self, amount):
+    self.amount = amount
 
-  def average(self):
-    return sum(self.marks) / len(self.marks)
+  def __repr__(self):
+    return f'<FixedFloat {self.amount:.2f}>'
 
-rolf = Student('Rolf', 'MIT')
-
-rolf.marks.append(78)
-rolf.marks.append(99)
-
-print(rolf.average())
-
-
-class Foo:
-  @classmethod
-  def hi(cls):
-    print(cls.__name__)
-
-my_object = Foo()
-my_object.hi()
-
-
-class Bar:
   @staticmethod
-  def hi():
-    print('Hello, I don\'t take parameters.')
+  def from_sum(value1, value2):
+    return FixedFloat(value1 + value2)
 
-# another_object = Bar()
-Bar.hi()
+new_number = FixedFloat.from_sum(19.575, 0.789)
+print(new_number)
+
+class Euro(FixedFloat):
+  def __init__(self, amount):
+    super().__init__(amount)
+    self.symbol = '₹'
+
+  def __repr__(self):
+    return f'<Euro {self.symbol}{self.amount:.2f}>'
+
+money = Euro(18.786)
+print(money)
