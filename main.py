b4d7e1eb06daea28e320085df58749b7707d311b
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-11 18:13:45 +0000

    another example

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ class FixedFloat:
   def __repr__(self):
     return f'<FixedFloat {self.amount:.2f}>'
 
+  def from_sum(self, value1, value2):
+    return FixedFloat(value1 + value2)
+
 number = FixedFloat(18.5746)
-print(number)
+new_number = number.from_sum(19.575, 0.789)
+print(new_number)
