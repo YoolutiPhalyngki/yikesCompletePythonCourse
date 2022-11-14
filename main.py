05fc34ecd780f55d14b1142dbc801ba9acb4b84c
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-14 12:38:00 +0000

    intro to errors in python

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-class Person:
-  def __init__(self, name):
-    self.name = name
+# Learning Error in Python
 
-  @staticmethod
-  def greet_friend(friend_name):
-    return f'Hey there, {friend_name}'
-
-print(Person.greet_friend('Mike'))
+print(my_variable)
