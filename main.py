f32d111d491b3f69140c35b4a6c23111f95b55fc
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-26 17:16:20 +0000

    Generator and Iterator example

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-# Learning Error in Python
+# Learning
 
-print(my_variable)
+class FirstHundredGenerator:
+  def __init__(self):
+    self.number = 0
+
+  def __next__(self):
+    if self.number < 100:
+      current = self.number
+      self.number += 1
+      return current
+    else:
+      raise StopIteration()
+
+my_gen = FirstHundredGenerator()
+print(next(my_gen))
+print(next(my_gen))
+print(next(my_gen))
+
+
+class FirstFiveIterator:
+  def __init__(self):
+    self.numbers = [1, 2, 3, 4, 5]
+    self.i = 0
+
+  def __next__(self):
+    if self.i < len(self.numbers):
+      current = self.numbers[self.i]
+      self.i += 1
+      return current
+    else:
+      raise StopIteration()
+
+my_iter = FirstFiveIterator()
+print(next(my_iter))
+print(next(my_iter))
