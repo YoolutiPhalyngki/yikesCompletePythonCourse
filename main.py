decc8c5d15e5c92393f8b63528282b3167c801af
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-27 08:31:27 +0000

    creating a custom filter function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+def my_custom_filter(func, iterable):
+  for i in iterable:
+    if func(i):
+      yield i
+
 friends = ['Rolf', 'Jose', 'Randy', 'Anna', 'Mary']
-start_with_r = filter(lambda friend: friend.startswith('R'), friends)
+start_with_r = my_custom_filter(lambda friend: friend.startswith('R'), friends)
 
 another_starts_with_r = (f for f in friends if f.startswith('R'))
 
-print(list(another_starts_with_r))
-print(list(another_starts_with_r))
+
+print(list(start_with_r))
+print(list(start_with_r))
