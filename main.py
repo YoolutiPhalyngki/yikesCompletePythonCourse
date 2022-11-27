ad7cf350ecc405690b67b7dfe22f6451a30ecb79
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-27 08:16:31 +0000

    filter example

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-class FirstHundredGenerator:
-  def __init__(self):
-    self.number = 0
+def starts_with_r(friend):
+  return friend.startswith('R')
 
-  def __next__(self):
-    if self.number < 100:
-      current = self.number
-      self.number += 1
-      return current
-    else:
-      raise StopIteration()
+friends = ['Rolf', 'Jose', 'Randy', 'Anna', 'Mary']
+start_with_r = filter(starts_with_r, friends)
 
-  def __iter__(self):
-    return self
-
-
-class AnotherIterable:
-  def __init__(self):
-    self.cars = ['Fiesta', 'Focus']
-
-  def __len__(self):
-    return len(self.cars)
-
-  def __getitem__(self, i):
-    return self.cars[i]
-
-
-for car in AnotherIterable():
-  print(car)
-
-my_numbers = [x for x in [1, 2, 3, 4, 5]]
-my_numbers_gen = (x for x in [1, 2, 3, 4, 5])
-
-print(next(my_numbers_gen))
+print(next(start_with_r))
+print(next(start_with_r))
+print(next(start_with_r))
