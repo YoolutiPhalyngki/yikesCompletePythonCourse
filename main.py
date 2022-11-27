ce4c1a131ca0e5c881be95d6be683d4056b3de01
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-27 08:25:01 +0000

    generator comprehension example identical to the filter function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 friends = ['Rolf', 'Jose', 'Randy', 'Anna', 'Mary']
 start_with_r = filter(lambda friend: friend.startswith('R'), friends)
 
-print(list(start_with_r))
-print(list(start_with_r))
+another_starts_with_r = (f for f in friends if f.startswith('R'))
+
+print(list(another_starts_with_r))
+print(list(another_starts_with_r))
