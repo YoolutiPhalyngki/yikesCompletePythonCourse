00477f647dacdb6bb77d7a5c6600c3b453e1e179
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-27 08:54:17 +0000

    creating a generator comprehension similar to map function.

--- main.py
+++ main.py
@@ -4,4 +4,7 @@ start_with_r = filter(lambda friend: friend.startswith('R'), friends)
 another_starts_with_r = (f for f in friends if f.startswith('R'))
 
 friends_lower = map(lambda x: x.lower(), friends)
+friends_lower = [friend.lower() for friend in friends]
+friends_lower = (friend.lower() for friend in friends)
+
 print(next(friends_lower))
