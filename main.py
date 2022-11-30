949e3d7567d04b88657b0d41b6fc557e451ee8e2
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-30 18:15:02 +0000

    default values for parameters

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ accounts = {
   'savings': 3695.50
 }
 
-def add_balance(amount: float, name: str) -> float:
+def add_balance(amount: float, name: str = 'checking') -> float:
   """Function to update the balance of an account and return the new balance."""
   accounts[name] += amount
   return accounts[name]
 
-add_balance(500.00, 'savings')
+add_balance(500.00)
 
-print(accounts['savings'])
+print(accounts['checking'])
