b659544a62d7ecafeff4ff924a85f3d788d30d29
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-30 18:48:40 +0000

    Use the default parameter as None, and then initialized the parameter to empty value if there is no parameter passed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def create_account(name: str, holder: str, account_holders):
-  print(id(account_holders))
+def create_account(name: str, holder: str, account_holders = None):
+  if not account_holders:
+    account_holders = []
+    
   account_holders.append(holder)
 
   return {
@@ -8,7 +10,7 @@ def create_account(name: str, holder: str, account_holders):
     'account_holders': account_holders
   }
 
-a1 = create_account('checking', 'Rolf', [])
-a2 = create_account('savings', 'Jen', [])
+a1 = create_account('checking', 'Rolf')
+a2 = create_account('savings', 'Jen')
 
 print(a2)
