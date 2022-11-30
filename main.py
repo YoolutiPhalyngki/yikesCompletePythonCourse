b20e45558afe3578c21e0e8044da44e15f4838c3
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-11-30 17:34:30 +0000

    Argument mutability

--- main.py
+++ main.py
@@ -4,38 +4,10 @@ friends_last_seen = {
   'Anne': 7
 }
 
-print(id(friends_last_seen))
-
-friends_last_seen = {
-  'Rolf': 31,
-  'Jen': 1,
-  'Anne': 7
-}
+def see_friend(friends, name):
+  print(id(friends))
+  friends[name] = 0
 
 print(id(friends_last_seen))
-
-friends_last_seen['Rolf'] = 0
-
+see_friend(friends_last_seen, 'Rolf')
 print(id(friends_last_seen))
-
-my_int = 5
-
-print(id(my_int))
-
-my_int = my_int + 1
-
-print(id(my_int))
-
-friends = ['Rolf', 'Jose']
-print(id(friends))
-
-friends.append('Jen')
-print(id(friends))
-
-"""
-Immutable:
-integers
-floats
-strings
-tuples
-"""
