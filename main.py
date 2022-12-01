d9442e30d5f6a47943e18f919370496ee46f0e84
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 06:43:51 +0000

    namedtuple example

--- main.py
+++ main.py
@@ -10,5 +10,10 @@ from collections import namedtuple
 
 account = ('checking', 1850.90)
 
-print(account[0])   # name
-print(account[1])   # balance
+Account = namedtuple('Account', ['name', 'balance'])
+
+account = Account('checking', 1850.90)
+print(account.name)
+
+print(account)
+
