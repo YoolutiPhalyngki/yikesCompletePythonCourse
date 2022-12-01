982ac7bda38be2962e13989464686d20c4f48555
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 05:46:31 +0000

    python collections intro

--- main.py
+++ main.py
@@ -1,54 +1,14 @@
-accounts = {
-  'checking': 1958.00,
-  'savings': 3695.50
-}
+"""
+* counter
+* defaultdict
+* ordereddict
+* namedtuple
+* deque
+"""
 
-def add_balance(amount: float, name: str) -> float:
-  """Function to update the balance of an account and return the new balance."""
-  accounts[name] += amount
-  return accounts[name]
+from collections import Counter
 
+device_temperatures = [13.5, 14.0, 14.0, 14.5, 14.5, 14.5, 15.0, 16.0]
 
-transactions = [
-  (-180.67, 'checking'),
-  (-220.00, 'checking'),
-  (220.00, 'savings'),
-  (-15.70, 'checking'),
-  (-23.90, 'checking'),
-  (-13.00, 'checking'),
-  (1579.50, 'checking'),
-  (-600.50, 'checking'),
-  (600.50, 'savings'),
-]
-
-for t in transactions:
-  add_balance(name=t[1], amount=t[0])
-
-
-
-class User:
-  def __init__(self, username, password):
-    self.username = username
-    self.password = password
-
-
-# imagine these users are coming from a database...
-users = [
-  {'username': 'rolf', 'password': '123'},
-  {'username': 'tecladoisawesome', 'password': 'youaretoo'}
-]
-
-user_objects = [User(data['username'], data['password']) for data in users]
-user_objects = [User(username=data['username'], password=data['password']) for data in users]
-user_objects = [User(**data) for data in users]
-
-print(user_objects)
-
-users = [
-  ('rolf', '123'),
-  ('tecladoisawesome', 'youaretoo')
-]
-
-user_objects = [User(*data) for data in users]
-
-print(user_objects)
+temperature_counter = Counter(device_temperatures)
+print(temperature_counter[14.0])
