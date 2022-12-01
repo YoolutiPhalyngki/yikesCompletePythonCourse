be3a392975fa648eb366b4d6fd2cfea6b09ce3e5
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 06:10:22 +0000

    defaultdict example

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ from collections import defaultdict
 
 coworkers = [('Rolf', 'MIT'), ('Jen', 'Oxford'), ('Rolf', 'Cambridge'), ('Charlie', 'Manchester')]
 
-alma_maters = {}
+alma_maters = defaultdict(list)
 
 for coworker, place in coworkers:
-  if coworker not in alma_maters:
-    alma_maters[coworker] = []
   alma_maters[coworker].append(place)
 
-print(alma_maters)
+# print(alma_maters)
+# print(alma_maters['Rolf'][1])
+# print(alma_maters['Anne'])
+
+alma_maters.default_factory = None
+
+print(alma_maters['Rolf'])
+print(alma_maters['Anne'])
