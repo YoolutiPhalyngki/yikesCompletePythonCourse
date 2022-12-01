291ae38689a70e2894efba85f6c688a484888b23
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 05:48:26 +0000

    another example on python collections Counter

--- main.py
+++ main.py
@@ -11,4 +11,6 @@ from collections import Counter
 device_temperatures = [13.5, 14.0, 14.0, 14.5, 14.5, 14.5, 15.0, 16.0]
 
 temperature_counter = Counter(device_temperatures)
-print(temperature_counter[14.0])
+print(temperature_counter[14.5])
+
+print(Counter({'hello': 5, 'hi': 3})['hi'])
