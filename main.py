4d76db108d35908cfac935e23324f02d2481ef59
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 17:52:45 +0000

    My solutions to the Python collections exercise

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
-"""
-* counter
-* defaultdict
-* ordereddict
-* namedtuple
-* deque
-"""
+from collections import defaultdict, OrderedDict, namedtuple, deque
 
-from collections import deque
+def task1() -> defaultdict:
+  my_dict = defaultdict(lambda: 'Unknown')
+  my_dict['Alan'] = 'Manchester'
 
-friends = deque(('Rolf', 'Charlie', 'Jen', 'Anna'))
-print(friends)
+  return my_dict
 
-friends.append('Jose')
-friends.appendleft('Anthony')
+def task2(arg_od: OrderedDict):
+  arg_od.popitem()
+  arg_od.popitem(last=False)
+  arg_od.move_to_end('Bob')
+  arg_od.move_to_end('Dan', last=False)
 
-print(friends)
+# my_dict = OrderedDict([
+#   ('Alan', 'Manchester'),
+#   ('Bob', 'London'),
+#   ('Chris', 'Lisbon'),
+#   ('Dan', 'Paris'),
+#   ('Eden', 'Liverpool'),
+#   ('Frank', 'Newcastle')
+# ])
 
-friends.pop()
-friends.popleft()
-
-print(friends)
+# task2(my_dict)
 
 
+def task3(name: str, club: str) -> namedtuple:
+  Player = namedtuple('Player',['name', 'club'])
+  playerNamedTuple = Player(name, club)
+  
+  return playerNamedTuple
+
+# player = ('Ronaldo', 'Manchester')
+
+# playerNamedTuple = task3(*player)
+# print(playerNamedTuple)
+
+
+def task4(arg_deque: deque):
+  arg_deque.pop()
+  arg_deque.append(arg_deque.popleft())
+  arg_deque.appendleft('Zack')
+
+# friends = deque(('Rolf', 'Mack', 'Jerry', 'Anna', 'Jose'))
+
+# task4(friends)
