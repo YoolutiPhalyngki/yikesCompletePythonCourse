1dce1ea5a2597bc782961e277cf8108a5ed293b0
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 17:56:56 +0000

    Another solution to Python collections exercise

--- main.py
+++ main.py
@@ -1,46 +1,27 @@
 from collections import defaultdict, OrderedDict, namedtuple, deque
-
+ 
+ 
 def task1() -> defaultdict:
-  my_dict = defaultdict(lambda: 'Unknown')
-  my_dict['Alan'] = 'Manchester'
-
-  return my_dict
-
+    dd = defaultdict(lambda: 'Unknown')
+    dd['Alan'] = 'Manchester'
+    return dd
+ 
+ 
 def task2(arg_od: OrderedDict):
-  arg_od.popitem()
-  arg_od.popitem(last=False)
-  arg_od.move_to_end('Bob')
-  arg_od.move_to_end('Dan', last=False)
-
-# my_dict = OrderedDict([
-#   ('Alan', 'Manchester'),
-#   ('Bob', 'London'),
-#   ('Chris', 'Lisbon'),
-#   ('Dan', 'Paris'),
-#   ('Eden', 'Liverpool'),
-#   ('Frank', 'Newcastle')
-# ])
-
-# task2(my_dict)
-
-
+    arg_od.popitem()
+    arg_od.popitem(False)
+    # remember to remove start and end before moving Bob and Dan, otherwise they will be removed instead
+    arg_od.move_to_end('Bob')
+    arg_od.move_to_end('Dan', False)
+ 
+ 
 def task3(name: str, club: str) -> namedtuple:
-  Player = namedtuple('Player',['name', 'club'])
-  playerNamedTuple = Player(name, club)
-  
-  return playerNamedTuple
-
-# player = ('Ronaldo', 'Manchester')
-
-# playerNamedTuple = task3(*player)
-# print(playerNamedTuple)
-
-
+    Player = namedtuple('Player', ['name', 'club'])
+    player = Player(name, club)
+    return player
+ 
+ 
 def task4(arg_deque: deque):
-  arg_deque.pop()
-  arg_deque.append(arg_deque.popleft())
-  arg_deque.appendleft('Zack')
-
-# friends = deque(('Rolf', 'Mack', 'Jerry', 'Anna', 'Jose'))
-
-# task4(friends)
+    arg_deque.pop()     # remove last element
+    arg_deque.append(arg_deque.popleft())   # remove first element and append it to last
+    arg_deque.appendleft('Zack')    # add Zack to start
