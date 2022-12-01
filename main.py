949dd24857b2bab245f0939080a56a477406193f
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-01 06:11:32 +0000

    defaultdict example

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ alma_maters = defaultdict(list)
 for coworker, place in coworkers:
   alma_maters[coworker].append(place)
 
-# print(alma_maters)
-# print(alma_maters['Rolf'][1])
-# print(alma_maters['Anne'])
+print(alma_maters)
+print(alma_maters['Rolf'][1])
+print(alma_maters['Anne'])
 
 alma_maters.default_factory = None
 
 print(alma_maters['Rolf'])
 print(alma_maters['Anne'])
+print(alma_maters['Jose'])
