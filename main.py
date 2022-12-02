442acecea7d2dcf66ced0919865495ee5b717454
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 15:00:41 +0000

    my solution to the exercise on secure file names using regex

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 import re
 
-price = 'Price: $18,649.50'
-expression = 'Price: \$([0-9,]*\.[0-9]*)'
-
-matches = re.search(expression, price)
-print(matches.group(0))  # entire match
-print(matches.group(1))  # first thing in brackets
-
-price_without_comma = matches.group(1).replace(',', '')
-price_number = float(price_without_comma)
-print(price_number)
+def is_filename_safe(filename):
+  regex = '^[a-zA-Z0-9]+[a-zA-Z0-9\-_\(\)]*\.(jpg|jpeg|png|gif)$'
+  return re.match(regex, filename) is not None
