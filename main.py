c1a8e495fc71c70731c1f76b09b7cbc3c3cd2400
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 06:56:59 +0000

    datetime examples

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ tomorrow = today + timedelta(days=1)
 print(today)
 print(tomorrow)
 
-print(today.strftime('%d-%m-%Y %H:%M.%f'))
+print(today.strftime('%d-%m-%Y %H:%M.%f'))  # string format time
 print(today.strftime('%F %T.%f'))
+
+user_date = input('Enter the date in YYYY-mm-dd format: ')
+user_date = datetime.strptime(user_date, '%Y-%m-%d')  # string parse time
+
+print(user_date)
+
+import time
+
+print(time.tzname)
