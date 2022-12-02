266a86328fb11a8729c588acc694654569c02658
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 14:31:28 +0000

    extract the floating price number from a string/text using regex and searc() groups

--- main.py
+++ main.py
@@ -6,3 +6,6 @@ expression = 'Price: \$([0-9]*\.[0-9]*)'
 matches = re.search(expression, price)
 print(matches.group(0))  # entire match
 print(matches.group(1))  # first thing in brackets
+
+price_number = float(matches.group(1))
+print(price_number)
