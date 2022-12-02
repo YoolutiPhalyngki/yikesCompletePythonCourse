d5355f9bdb614ec0dc444b1374b3986570a1158a
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 14:27:19 +0000

    group(0) is the entire match and group(1) is the first thing in brackets

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ price = 'Price: $189.50'
 expression = 'Price: \$(189.50)'
 
 matches = re.search(expression, price)
-print(matches.group(0))
-print(matches.group(1))
+print(matches.group(0))  # entire match
+print(matches.group(1))  # first thing in brackets
