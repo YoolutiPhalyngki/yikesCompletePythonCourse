e032058d235751bae8a9afbc69b50010b6b73ee6
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 14:35:44 +0000

    use replace() string method to remove the comma from a number that has comma and follow by coverting it to float

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import re
 
-price = 'Price: $18649.50'
-expression = 'Price: \$([0-9]*\.[0-9]*)'
+price = 'Price: $18,649.50'
+expression = 'Price: \$([0-9,]*\.[0-9]*)'
 
 matches = re.search(expression, price)
 print(matches.group(0))  # entire match
 print(matches.group(1))  # first thing in brackets
 
-price_number = float(matches.group(1))
+price_without_comma = matches.group(1).replace(',', '')
+price_number = float(price_without_comma)
 print(price_number)
