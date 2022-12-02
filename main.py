84f34b3ced074e65faf0643e308afae62988fc3b
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 15:04:25 +0000

    modified version of the solution on secure file names using regex

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
 
 def is_filename_safe(filename):
-  regex = '^[a-zA-Z0-9]+[a-zA-Z0-9\-_\(\)]*\.(jpg|jpeg|png|gif)$'
+  regex = '^[a-zA-Z0-9][a-zA-Z0-9_()-]*\.(jpg|jpeg|png|gif)$'
   return re.match(regex, filename) is not None
