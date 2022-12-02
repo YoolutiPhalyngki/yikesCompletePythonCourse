53072fc14b9f9ac18a8dc9bf263f9515dfab8d8f
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-02 14:15:00 +0000

    regex in action

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import re
 
 email = 'jose@tecladocode.com'
-expression = '[a-z]+'
+expression = '[a-z\.]+'
 
 matches = re.findall(expression, email)
 print(matches)
 
 name = matches[0]
-domain = f'{matches[1]}.{matches[2]}'
+domain = matches[1]
 
 print(name)
 print(domain)
