66e8baa66c57eb159af07f7011128bff3828a499
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-03 19:07:42 +0000

    include datefmt in the logging config

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 
 logging.basicConfig(
   format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', 
+  datefmt='%d-%m-%Y %H:%M:%S',
   level=logging.DEBUG,
   filename='logs.txt'
 )
