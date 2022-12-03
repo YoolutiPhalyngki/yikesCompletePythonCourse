7e75de257f5b1f9bc1a0732b8cceec5928ef817a
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-03 18:40:32 +0000

    configure the logging in Python

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import logging
 
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger('test_logger')
 
 logger.info('This will not show up.')
 logger.warning('This will.')
+
+"""
+DEBUG
+INFO
+WARNING
+ERROR
+CRITICAL
+"""
