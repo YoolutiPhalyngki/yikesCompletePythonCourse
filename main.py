b45e89b62e501a8aca82a455186e57552c2d830a
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-03 19:11:59 +0000

    intro to higher-order functions

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
-import logging
+from typing import Callable
 
-logging.basicConfig(
-  format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s', 
-  datefmt='%d-%m-%Y %H:%M:%S',
-  level=logging.DEBUG,
-  filename='logs.txt'
-)
-logger = logging.getLogger('test_logger')
 
-logger.info('This will not show up.')
-logger.warning('This will.')
-logger.debug('This is a debug message.')
-logger.critical('A critical error occurred.')
+def greet():
+  print('Hello')
 
-"""
-DEBUG
-INFO
-WARNING
-ERROR
-CRITICAL
-"""
+
+def before_and_after(func: Callable):
+  print("Before...")
+  func()
+  print("After...")
+
+
+before_and_after(5)
