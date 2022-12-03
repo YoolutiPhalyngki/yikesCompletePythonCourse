3f4301f0adc8e0858bc306e4f95e31cc73f238a6
YoolutiPhalyng1 <lutiyoo@rediffmail.com>
2022-12-03 19:27:25 +0000

    example on higher-order functions

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from typing import Callable
 
-
-def greet():
-  print('Hello')
-
-
-def before_and_after(func: Callable):
-  print("Before...")
-  func()
-  print("After...")
+movies = [
+  {"name": "The Matrix", "director": "Wachowski"},
+  {"name": "A Beautiful Day in the Neighborhood", "director": "Heller"},
+  {"name": "The Irishman", "director": "Scorsese"},
+  {"name": "Klaus", "director": "Pablos"},
+  {"name": "1917", "director": "Mendes"},
+]
 
 
-before_and_after(greet)
+def find_movie(finder: Callable):
+  for movie in movies:
+    print(finder(movie))
+
+find_by = input("What property are we searching by? ")
+looking_for = input("What are you looking for? ")
+find_movie(lambda movie: movie[find_by])
